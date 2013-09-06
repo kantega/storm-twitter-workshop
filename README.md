1440d1fe514fe28620277d4085f75727748fb49f
Qadeer Ahmad <qadeer@Qadeers-MacBook-Pro.local>
2013-09-06 13:58:31 +0200

    Changed TwitterSpout to use Twitter4j StatusListener instead of raw twitter data to enable fetching only tweet messages. Parameterized the main method so that twitter credentials can be provided through maven command. Documented how to run TwitterSpout

---
+++ README.md
@@ -0,0 +1,12 @@
+## Quick start ##
+To run TwitterSpout: 
+``mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=TwitterSpout -Dexec.args="CONSUMER\_KEY, CONSUMER\_SECRET, ACCESS\_TOKEN, ACCESS\_TOKEN\_SECRET" ``
+... where CONSUMER\_KEY, CONSUMER\_SECRET, ACCESS\_TOKEN, ACCESS\_TOKEN\_SECRET are values from your twitter developer account. For details see below.
+
+## Create keys and tokens ##
+1. Go to [Twitter developer site](https://dev.twitter.com) and log in. Create new account if you do not already have one
+2. Click on the arrow on top right beside your account image and select "My applications".
+3. Create a new application. If you already have one, you can use credentials from that application and skip this and the next step.
+4. Fill in the details. You can use dummy values for all required fields.
+5. In the application detail page note the **Consumer key** and **Consumer secret** fields under OAuth settings
+6. Create a new access token at the buttom of the page. The **Access token** and **Access token secret** will be shown after a moment.
--- src/TwitterSpout.java
+++ src/TwitterSpout.java
@@ -7,9 +7,7 @@ import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
-import twitter4j.RawStreamListener;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 import backtype.storm.Config;
 
@@ -17,29 +15,43 @@ import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
+ *
+ * TwitterSpout fetches messages from Twitter Streaming API using Twitter4j.
  * Created with IntelliJ IDEA.
  * User: qadeer
  * Date: 05.09.13
  * Time: 22:50
- * To change this template use File | Settings | File Templates.
+ *
  */
 public class TwitterSpout extends BaseRichSpout {
-    private static final String CONSUMER_KEY = "";
-    private static final String CONSUMER_SECRET = "";
-    private static final String ACCESS_TOKEN = "";
-    private static final String ACCESS_SECRET = "";
-    private String _username;
-    private String _pwd;
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessToken;
+    private String accessTokenSecret;
+    private String username;
+    private String password;
+
     private SpoutOutputCollector _collector;
     private TwitterStream _twitterStream;
     private LinkedBlockingQueue _msgs;
 
-    public TwitterSpout(String username, String pwd) {
+    public TwitterSpout(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
+        if (consumerKey==null ||
+                consumerSecret==null ||
+                accessToken ==null ||
+                accessTokenSecret == null){
+            throw new RuntimeException("Twitter4j OAuth field cannot be null");
+        }
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+        this.accessToken = accessToken;
+        this.accessTokenSecret = accessTokenSecret;
 
-        _username = username;
-        _pwd = pwd;
+    }
 
-
+    public TwitterSpout(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
     @Override
@@ -47,20 +59,44 @@ public class TwitterSpout extends BaseRichSpout {
         outputFieldsDeclarer.declare(new Fields("msg"));
     }
 
+    /**
+     * Creates a twitter stream listener which adds messages to a LinkedBlockingQueue.
+
+     */
     @Override
     public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
         _msgs = new LinkedBlockingQueue();
         _collector = spoutOutputCollector;
         ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setOAuthConsumerKey(CONSUMER_KEY)
-                .setOAuthConsumerSecret(CONSUMER_SECRET)
-                .setOAuthAccessToken(ACCESS_TOKEN)
-                .setOAuthAccessTokenSecret(ACCESS_SECRET);
+            cb.setOAuthConsumerKey(consumerKey)
+                    .setOAuthConsumerSecret(consumerSecret)
+                    .setOAuthAccessToken(accessToken)
+                    .setOAuthAccessTokenSecret(accessTokenSecret);
         _twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
-        _twitterStream.addListener(new RawStreamListener() {
+        _twitterStream.addListener(new StatusListener() {
             @Override
-            public void onMessage(String rawString) {
-                _msgs.offer(rawString);
+            public void onStatus(Status status) {
+                    _msgs.offer(status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                //To change body of implemented methods use File | Settings | File Templates.
             }
 
             @Override
@@ -71,9 +107,11 @@ public class TwitterSpout extends BaseRichSpout {
         _twitterStream.sample();
 
 
-        // connect to twitter
     }
 
+    /**
+     * When requested for next tuple, reads message from queue
+     */
     @Override
     public void nextTuple() {
         // emit tweets
@@ -87,8 +125,9 @@ public class TwitterSpout extends BaseRichSpout {
     }
 
     public static void main(String[] args){
+
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("twitterSpout", new TwitterSpout(null,null),1);
+        builder.setSpout("twitterSpout", new TwitterSpout(args[0],args[1],args[2],args[3]),1);
         LocalCluster cluster = new LocalCluster();
         Config conf = new Config();
         conf.setDebug(true);
